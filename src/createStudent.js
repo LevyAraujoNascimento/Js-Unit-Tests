79bf2a3b92d24424401857c38955bd056b449a50
Levy Araujo <levy4raujo@gmail.com>
2022-10-21 11:03:38 -0300

    5º requisito test Lint 1

--- src/createStudent.js
+++ src/createStudent.js
@@ -18,9 +18,7 @@
 const createStudent = (nome) => {
   const estudantes = {
     name: nome,
-    feedback: () => {
-      return 'Eita pessoa boa!'; 
-    }, 
+    feedback: () => 'Eita pessoa boa!', 
   };
   return estudantes;
 };
